43016a03c30f1ca92c62a2af80872629e81579db
Josh Watzman <sinclair44@gmail.com>
2008-05-25 17:03:50 -0400

    warriorness correctly asserts that this is equivalent and faster

--- bitboard.h
+++ bitboard.h
@@ -3,7 +3,7 @@
 // for all of these conversions, 0 <= row,col < 8
 static inline uint8_t bb_index_of(uint8_t row, uint8_t col)
 {
-	return row + (col << 3);
+	return row | (col << 3);
 }
 
 static inline uint8_t bb_row_of(uint8_t index)
