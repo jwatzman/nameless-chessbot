902e3dd0e8f437a539e12462ef92b529939e4541
Josh Watzman <sinclair44@gmail.com>
2008-05-26 21:40:23 -0400

    Beginnings of the bitboard manipulation functions

---
+++ bitboard.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <bitboard.h>
+
+Bitboard init_starting_position()
+{
+	Bitboard result;
+
+	return result;
+}
+
+uint64_t bb_rotate_90()
+{
+}
+
+uint64_t bb_rotate_45()
+{
+}
+
+uint64_t bb_rotate_315()
+{
+}
--- bitboard.h
+++ bitboard.h
@@ -1,4 +1,30 @@
+#ifndef _BITBOARD_H
+#define _BITBOARD_H
+
 #include <stdint.h>
+
+enum Color { WHITE=0, BLACK };
+enum Piecetype { PAWN=0, BISHOP, KNIGHT, ROOK, QUEEN, KING };
+
+typedef struct
+{
+	uint64_t boards[2][6];
+	uint64_t boards45[2][6];
+	uint64_t boards90[2][6];
+	uint64_t boards315[2][6];
+
+	uint64_t composite_white;
+	uint64_t composite_black;
+
+	uint8_t has_castled; // bit 0 set iff white castled, bit 1 iff black
+	// other data will probably have to go here
+}
+Bitboard;
+
+Bitboard bb_init_starting_position();
+uint64_t bb_rotate_90(uint64_t board);
+uint64_t bb_rotate_45(uint64_t board);
+uint64_t bb_rotate_315(uint64_t board);
 
 // for all of these conversions, 0 <= row,col < 8
 static inline uint8_t bb_index_of(uint8_t row, uint8_t col)
@@ -15,3 +41,5 @@ static inline uint8_t bb_col_of(uint8_t index)
 {
 	return (uint8_t)(index / 8);
 }
+
+#endif
