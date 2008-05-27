38c731b4b8f969b5abf47d284c3f16790a9cfc50
jwatzman <jwatzman@andrew.cmu.edu>
2008-05-27 17:37:44 -0400

    Implement rotate function and tables for 90 degree rotation indices

--- bitboard.c
+++ bitboard.c
@@ -7,11 +7,23 @@
 
 // to get the rotated index of the unrotated index 0 <= n < 64, you would say
 // board_index_90[n] for example
-static const uint8_t board_rotation_index_90[] = {};
-static const uint8_t board_rotation_index_45[] = {};
-static const uint8_t board_rotation_index_315[] = {};
+// also, remember that these are upside-down, since a1/LSB must come first
+static const uint8_t board_rotation_index_90[64] = {
+7, 15, 23, 31, 39, 47, 55, 63,
+6, 14, 22, 30, 38, 46, 54, 62,
+5, 13, 21, 29, 37, 45, 53, 61,
+4, 12, 20, 28, 36, 44, 52, 60,
+3, 11, 19, 27, 35, 43, 51, 59,
+2, 10, 18, 26, 34, 42, 50, 58,
+1, 9, 17, 25, 33, 41, 49, 57,
+0, 8, 16, 24, 32, 40, 48, 56
+};
 
-uint64_t board_rotate_internal(uint64_t board, uint8_t rotation_index[64]);
+static const uint8_t board_rotation_index_45[64] = {};
+
+static const uint8_t board_rotation_index_315[64] = {};
+
+uint64_t board_rotate_internal(uint64_t board, const uint8_t rotation_index[64]);
 
 Bitboard board_init()
 {
@@ -38,14 +50,14 @@ Bitboard board_init()
 	{
 		for (int piece = PAWN; piece <= KING; piece++)
 		{
-			uint64_t to_be_rotated = boards[color][piece];
+			uint64_t to_be_rotated = result.boards[color][piece];
 			result.boards90[color][piece] = board_rotate_90(to_be_rotated);
 			result.boards45[color][piece] = board_rotate_45(to_be_rotated);
-			result.boards315[color][piece] = board_rotate_3115(to_be_rotated);
+			result.boards315[color][piece] = board_rotate_315(to_be_rotated);
 		}
 	}
 
-	result.castle_status = 0xFF00; // we can castle, but have not yet
+	result.castle_status = 0xF0; // we can castle, but have not yet
 
 	return result;
 }
@@ -65,8 +77,22 @@ uint64_t board_rotate_315(uint64_t board)
 	return board_rotate_internal(board, board_rotation_index_315);
 }
 
-uint64_t board_rotate_internal(uint64_t board, uint8_t rotation_index[64])
+uint64_t board_rotate_internal(uint64_t board, const uint8_t rotation_index[64])
 {
+	uint64_t result = 0;
+	static const uint64_t bit = 1;
+	uint8_t current_index = 0;
+
+	while (board)
+	{
+		if (board & bit)
+			result |= bit << rotation_index[current_index];
+
+		board >>= 1;
+		current_index++;
+	}
+
+	return result;
 }
 
 void board_print(Bitboard board)
