cd9083de1b723f7cf85533790dbf4e1eb456b3a0
Josh Watzman <sinclair44@gmail.com>
2008-05-26 20:35:59 -0400

    Diagonal generators, which appear to agree with warriorness's data.

---
+++ bb-tablegen-diag315.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "bitboard.h"
+
+uint64_t up_right_attack(int position_mask, int pos)
+{
+	uint64_t result = 0;
+
+	for (int cur_pos = pos + 1; cur_pos < 8; cur_pos++)
+	{
+		result |= (1ULL << bb_index_of(cur_pos, cur_pos));
+
+		if (position_mask & (1 << cur_pos))
+			break;
+	}
+	
+	return result;
+}
+
+uint64_t down_left_attack(int position_mask, int pos)
+{
+	uint64_t result = 0;
+
+	for (int cur_pos = pos - 1; cur_pos >= 0; cur_pos--)
+	{
+		result |= (1ULL << bb_index_of(cur_pos, cur_pos));
+
+		if (position_mask & (1 << cur_pos))
+			break;
+	}
+
+	return result;
+}
+
+int main(int argc, char** argv)
+{
+	printf("uint64_t diag_attacks_315[256][8] = {\n");
+
+	for (int position_mask = 0; position_mask < 256; position_mask++)
+	{
+		printf("\t{");
+
+		for (int pos = 0; pos < 8; pos++)
+		{
+			uint64_t attack_mask = up_right_attack(position_mask, pos);
+			attack_mask |= down_left_attack(position_mask, pos);
+
+			printf("0x%.16llx", attack_mask);
+			if (pos < 7) printf(", ");
+		}
+
+		printf("}");
+		if (position_mask < 255) printf(",");
+		printf("\n");
+	}
+
+	printf("};\n");
+}
