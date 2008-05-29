58106c41237d006a406f9b735dbbae9be6837a76
Josh Watzman <sinclair44@gmail.com>
2008-05-29 17:44:08 -0400

    In a trivial test, this appears to approximately work

--- bitboard-test.c
+++ bitboard-test.c
@@ -1,10 +1,19 @@
 #include <stdlib.h>
 #include "bitboard.h"
+#include "move.h"
 
 int main(int argc, char** argv)
 {
 	Bitboard *test = malloc(sizeof(Bitboard));
 	board_init(test);
+	
+	Move test_move = 0;
+	test_move |= board_index_of(1, 4);
+	test_move |= board_index_of(3, 4) << 6;
+	test_move |= PAWN << 12;
+	test_move |= WHITE << 15;
+
+	board_do_move(test, &test_move);
 
 	board_print(test->boards);
 	board_print(test->boards90);
