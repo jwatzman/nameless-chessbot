5ecceec194b46bda525811cb693ae9efcbe8d07d
jwatzman <jwatzman@andrew.cmu.edu>
2008-05-31 19:40:47 -0400

    Add constants for offsets into the move bytes

--- move.h
+++ move.h
@@ -11,54 +11,64 @@ void move_generate_movelist(Bitboard *board, Color to_move, Movelist *movelist);
 // verifies a pseudomove, returns 1 if it's valid, 0 otherwise
 int move_verify(Bitboard *board, Move move);
 
+static const uint8_t move_source_index_offset = 0;
 static inline uint8_t move_source_index(Move move)
 {
 	return move & 0x3F;
 }
 
+static const uint8_t move_destination_index_offset = 6;
 static inline uint8_t move_destination_index(Move move)
 {
-	return (move >> 6) & 0x3F;
+	return (move >> move_destination_index_offset) & 0x3F;
 }
 
+static const uint8_t move_piecetype_offset = 12;
 static inline Piecetype move_piecetype(Move move)
 {
-	return (move >> 12) & 0x07;
+	return (move >> move_piecetype_offset) & 0x07;
 }
 
+static const uint8_t move_color_offset = 15;
 static inline Color move_color(Move move)
 {
-	return (move >> 15) & 0x01;
+	return (move >> move_color_offset) & 0x01;
 }
 
+static const uint8_t move_is_castle_offset = 16;
 static inline int move_is_castle(Move move)
 {
-	return (move >> 16) & 0x01;
+	return (move >> move_is_castle_offset) & 0x01;
 }
 
+static const uint8_t move_is_enpassant_offset = 17;
 static inline int move_is_enpassant(Move move)
 {
-	return (move >> 17) & 0x01;
+	return (move >> move_is_enpassant_offset) & 0x01;
 }
 
+static const uint8_t move_is_capture_offset = 18;
 static inline int move_is_capture(Move move)
 {
-	return (move >> 18) & 0x01;
+	return (move >> move_is_capture_offset) & 0x01;
 }
 
+static const uint8_t move_is_promotion_offset = 19;
 static inline int move_is_promotion(Move move)
 {
-	return (move >> 19) & 0x01;
+	return (move >> move_is_promotion_offset) & 0x01;
 }
 
+static const uint8_t move_captured_piecetype_offset = 20;
 static inline int move_captured_piecetype(Move move)
 {
-	return (move >> 20) & 0x07;
+	return (move >> move_captured_piecetype_offset) & 0x07;
 }
 
+static const uint8_t move_promoted_piecetype_offset = 23;
 static inline int move_promoted_piecetype(Move move)
 {
-	return (move >> 23) & 0x07;
+	return (move >> move_promoted_piecetype_offset) & 0x07;
 }
 
 static inline void move_srcdest_form(Move move, char srcdest_form[5])
