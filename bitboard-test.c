a07124c50e791e68fff113c057302679f58d9bd4
jwatzman <jwatzman@andrew.cmu.edu>
2008-05-31 23:53:43 -0400

    You can now play a game of chess with the test driver, though it's implemented in a very braindead way (and major functionality is missing...)

--- bitboard-test.c
+++ bitboard-test.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "bitboard.h"
 #include "move.h"
 
@@ -23,24 +24,45 @@ int main(int argc, char** argv)
 	board_do_move(test, test_move);
 	*/
 
-	board_print(test->boards);
-
 	Movelist moves;
-	move_generate_movelist(test, &moves);
 	char* srcdest_form = malloc(5 * sizeof(char));
+	char* input_move = malloc(5 * sizeof(char));
 
-	for (int i = 0; i < moves.num; i++)
+	while (1)
 	{
-		Move move = moves.moves[i];
-		if (move_verify(test, move))
+		board_print(test->boards);
+		move_generate_movelist(test, &moves);
+
+		for (int i = 0; i < moves.num; i++)
 		{
-			move_srcdest_form(moves.moves[i], srcdest_form);
-			printf("%s ", srcdest_form);
+			Move move = moves.moves[i];
+			if (move_verify(test, move))
+			{
+				move_srcdest_form(move, srcdest_form);
+				printf("%s ", srcdest_form);
+			}
+		}
+		printf("\n");
+
+		scanf("%4s", input_move);
+
+		for (int i = 0; i < moves.num; i++)
+		{
+			Move move = moves.moves[i];
+			if (move_verify(test, move))
+			{
+				move_srcdest_form(move, srcdest_form);
+				if (!strcmp(input_move, srcdest_form))
+				{
+					board_do_move(test, move);
+					break;
+				}
+			}
 		}
 	}
-	printf("\n");
 
 	free(srcdest_form);
+	free(input_move);
 	free(test);
 	return 0;
 }
