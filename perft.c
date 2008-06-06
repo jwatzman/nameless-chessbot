3769610f4b685fbb7f52076faaaa572acf7b5415
jwatzman <jwatzman@andrew.cmu.edu>
2008-06-06 10:51:51 -0400

    Simple perft

---
+++ perft.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "bitboard.h"
+#include "move.h"
+
+Bitboard *board;
+unsigned long int nodes;
+
+void perft(int depth);
+
+int main(int argc, char** argv)
+{
+	board = malloc(sizeof(Bitboard));
+	nodes = 0;
+	int max_depth = atoi(argv[1]);
+	
+	board_init(board);
+
+	perft(max_depth);
+
+	printf("%lu nodes\n", nodes);
+	return 0;
+}
+
+void perft(int depth)
+{
+	if (depth == 0)
+	{
+		nodes++;
+		return;
+	}
+
+	Movelist moves;
+	move_generate_movelist(board, &moves);
+
+	for (int i = 0; i < moves.num; i++)
+	{
+		Move m = moves.moves[i];
+		board_do_move(board, m);
+
+		if (!board_in_check(board, 1-board->to_move))
+			perft(depth - 1);
+
+		board_undo_move(board, m);
+	}
+}
