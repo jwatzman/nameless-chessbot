beea552fdb9472a9691877f39fb584407c45320a
jwatzman <jwatzman@andrew.cmu.edu>
2008-08-19 19:13:13 -0400

    Add some comments to evaluate.c

--- evaluate.c
+++ evaluate.c
@@ -4,6 +4,8 @@
 #include "bitboard.h"
 #include "move.h"
 
+// position bonuses; remember that
+// since a1 is the first number, it is in the upper left
 static const int rook_pos[] = {
 0, 3, 7,  8,  8, 7, 3, 0,
 0, 3, 7,  8,  8, 7, 3, 0,
@@ -37,6 +39,8 @@ static const int knight_pos[] = {
 -10, -7, -5, -5, -5, -5, -7,-10
 };
 
+// the only one that isn't hoizontally symmetric;
+// this needs to be special-cased, see below
 static const int pawn_pos[] = {
 0,  0,  0,  0,  0,  0,  0,  0,
 0,  0,  0, -9, -9,  0,  0,  0,
@@ -48,9 +52,14 @@ static const int pawn_pos[] = {
 0,  0,  0,  0,  0,  0,  0,  0
 };
 
-// pawn, bishop, knight, rook, queen, king
+// metatable
 static const int* pos_tables[] = { pawn_pos, bishop_pos, knight_pos, rook_pos, 0, 0 };
+
+// piece intrinsic values, in order of:
+// pawn, bishop, knight, rook, queen, king
+// (as defined in types.h)
 static const int values[] = { 100, 300, 300, 500, 900, 0 };
+
 static const int castle_bonus = 10;
 
 static int popcnt(uint64_t x);
@@ -62,8 +71,13 @@ int evaluate_board(Bitboard *board)
 
 	for (Color color = 0; color < 2; color++)
 	{
+		// add values for the current player, and subtract them for the opponent
 		int modifier = (color == to_move ? 1 : -1);
 
+		// 5 == 00000101
+		// iff white has castled, exactly one of those bits will be set
+		// iff black has castled, exactly one of the bits to the left of one of those bits will be set
+		// (see description of castle_status in types.h)
 		if (board->castle_status & (5 << color))
 			result += modifier * castle_bonus;
 
@@ -75,14 +89,17 @@ int evaluate_board(Bitboard *board)
 				uint8_t loc = ffsll(pieces) - 1;
 				pieces ^= 1ULL << loc;
 
+				// since the pawn table is not hoizontally symmetric, we need to flip it for black
 				if (piece == PAWN && color == BLACK)
 					loc = 63 - loc;
 
+				// add in piece intrinsic value, and bonus for its location
 				const int *table = pos_tables[piece];
 				if (table)
 					result += modifier * table[loc];
 				result += modifier * values[piece];
 
+				// add in a bonus for every square that this piece can attack
 				uint64_t attacks = move_generate_attacks(board, piece, color, loc);
 				result += modifier * popcnt(attacks);
 			}
