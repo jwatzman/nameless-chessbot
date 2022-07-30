7e03d384640b7650cd44addf788fde24f48bb1fc
Josh Watzman <github@jwatzman.org>
2022-07-30 18:00:40 +0100

    Make sure the move we're pruning in qsearch is a capture

--- src/search.c
+++ src/search.c
@@ -440,7 +440,7 @@ static int search_qsearch(Bitboard* board, int alpha, int beta, int8_t ply) {
     // Ugh, this is really awful coupling -- relying on moveiter scoring
     // captures at 0 by default, so this is a working check for losing captures.
     // Definitely need a better factoring here!
-    if (!in_check && score < 0)
+    if (!in_check && move_is_capture(move) && score < 0)
       continue;
 #else
     Move move = moveiter_next(&iter, NULL);
